05e2b7330a153cb6962044843f6052792bce9966
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-14 11:03:37 +0530

    using react props gives imgurl, tags and username

--- 04tailwind-props/src/App.jsx
+++ 04tailwind-props/src/App.jsx
@@ -16,8 +16,13 @@ function App() {
   return (
     <>
       <h1>Welcome To TailwindCss with Props</h1>
-      <Card userName="GauravsSah" tags={tagsObj} />
-      <Card userName="Sah" tags={tagsObj} />
+      {/* <Card userName="GauravsSah" tags={tagsObj} /> */}
+      <Card imgUrl="https://images.pexels.com/photos/165505/pexels-photo-165505.jpeg?auto=compress&cs=tinysrgb&w=600" userName="GauravsSah" tag1="winter" tag2="Summer" tag3="Travel" />
+
+      <Card imgUrl="https://images.pexels.com/photos/3998365/pexels-photo-3998365.png?auto=compress&cs=tinysrgb&w=600" userName="GauravsSah" tag1="fun" tag2="time" tag3="Travel" />
+
+      <Card imgUrl="https://images.pexels.com/photos/1133502/pexels-photo-1133502.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" userName="GauravsSah" tag1="fun" tag2="time" tag3="Travel" />
+
     </>
   )
 }
