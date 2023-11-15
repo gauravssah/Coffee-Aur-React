fae7eef6ff70cf46e79e87b56631775827152526
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-15 14:54:41 +0530

    random password generator done

--- 06password-generator/src/App.jsx
+++ 06password-generator/src/App.jsx
@@ -1,62 +1,82 @@
-import { useState } from "react"
+import { useState, useCallback, useEffect, useRef } from "react"
 
 function App() {
 
-  let srt = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  let num = "0123456789";
-  let symb = "~!@#$%^&*";
-  let pass;
+  const [length, setLength] = useState(6);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
-  let [length, setLength] = useState(6);
-  let [numChecked, setNumChecked] = useState(false);
-  let [symbChecked, setSymbChecked] = useState(false);
-  let [password, setPassword] = useState();
+  const passwordGenerator = useCallback(() => {
 
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxtz";
 
-  const handleRange = (e) => {
-    setLength(e.target.value)
-  }
-
-  const generatePassword = () => {
-    if (numChecked) {
-      srt += num;
-    }
-    if (symbChecked) {
-      srt += symb;
+    if (numberAllowed) {
+      str += "0123456789";
     }
 
-    for (let i = 0; i < length; i++) {
-      let char = srt[Math.floor(Math.random() * length)];
+    if (charAllowed) {
+      str += "~!@#$%^&*(){}[]'<>.,/?+=-_";
+    }
 
-      password += char;
+    // ----------------------------
 
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
 
     }
 
+    setPassword(pass)
 
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
-  }
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
 
-  generatePassword()
+  // useRef Hook 
+
+  const passwordRef = useRef(null);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0, 999); // this is optional for selection any specifice range of text.
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
 
   return (
     <>
       <div className="container bg-warning mt-5 w-50 p-5">
         <h2 className="text-center">Password Generator</h2>
+
         <div className="container d-flex">
-          <input type="text" className="form-control" placeholder="Password" value={password} readOnly />
-          <button type="submit" className="btn btn-primary mx-2">Copy</button>
+          <input type="text" className="form-control" placeholder="Password" value={password} readOnly ref={passwordRef} />
+
+          <button onClick={copyPasswordToClipboard} type="submit" className="btn btn-primary mx-2">Copy</button>
         </div>
 
         <div className="container text-center mt-3">
 
-          <input onChange={handleRange} type="range" name="range" id="range" min={6} max={100} />
-          <label className="mx-2" htmlFor="range">Length {length}</label>
+          <input onChange={(e) => {
+            setLength(e.target.value);
+          }}
+            type="range" name="range" id="range" min={6} max={100} value={length} />
 
-          <input type="checkbox" name="checknumber" id="checknumber" />
+          <label className="mx-2" htmlFor="range">Length : {length}</label>
+
+          <input onChange={() => {
+            setNumberAllowed((prev) => !prev);
+          }}
+            type="checkbox" name="checknumber" id="checknumber" defaultChecked={numberAllowed} />
           <label className="mx-2" htmlFor="checknumber">Number</label>
 
-          <input type="checkbox" name="checksymble" id="checksymble" />
+          <input onChange={() => {
+            setCharAllowed((prev) => !prev);
+          }}
+            type="checkbox" name="checksymble" id="checksymble" defaultChecked={charAllowed} />
           <label className="mx-2" htmlFor="checksymble">Symble</label>
 
         </div>
