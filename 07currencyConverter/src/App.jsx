60a850e98a27122a6a95c7e78dff9fcc06ba917d
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-11-16 19:08:05 +0530

    currencty component created

---
+++ 07currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { InputBox } from './components';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500 '>Currency app Using React</h1>
+    </>
+  )
+}
+
+export default App
